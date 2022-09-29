4f2eec068c7e42dd1ba90c5ad948f7bbf034dcce
sayrish <sayrrish1@gmail.com>
2022-09-29 10:17:24 +0530

    Added: Employee Daily Wage Logic

--- src/Com/Wage/EmployeeWage.java
+++ src/Com/Wage/EmployeeWage.java
@@ -6,18 +6,22 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		
-		System.out.println("welcome to emplyee wage");
-		Random r = new Random();
-		int random = r.nextInt(2);
-		if(random == 1)
+		int empWagePerHour = 20;
+		int fullDayHour = 8;
+		int salary = 0;
+		System.out.println("Welcome to Employee Wage Computation Program");
+		Random random = new Random();
+		int empCheck = random.nextInt(2);
+		if(empCheck == 1)
 		{
-			System.out.println("present");
-			
+			System.out.println("Employee is Present.");
+			salary = empWagePerHour * fullDayHour ;
 		}
-		else {
-			System.out.println("absent");
+		else
+		{
+			System.out.println("Employee is Absent.");
 		}
-			
+		System.out.println("Salary is: "+salary);
 		
 		
 	}
