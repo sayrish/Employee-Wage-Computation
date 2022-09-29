80a22e4c264db96eed3b1c57012955e3c411a4c2
sayrish <sayrrish1@gmail.com>
2022-09-29 10:11:52 +0530

    Added: employee present/absent logic

--- src/Com/Wage/EmployeeWage.java
+++ src/Com/Wage/EmployeeWage.java
@@ -1,11 +1,25 @@
 package Com.Wage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		
 		System.out.println("welcome to emplyee wage");
-
+		Random r = new Random();
+		int random = r.nextInt(2);
+		if(random == 1)
+		{
+			System.out.println("present");
+			
+		}
+		else {
+			System.out.println("absent");
+		}
+			
+		
+		
 	}
 
 }
